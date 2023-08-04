cc8aed56c2ac82305bea419b60886969943fe845
Samir Gupta <100823636+samirg1@users.noreply.github.com>
2023-08-04 15:38:18 +1000

    Delete build/lib directory

--- build/lib/design/__init__.py
+++
@@ -1,7 +0,0 @@
-# pyright: reportUnusedImport=false
-from design.data import SCRIPTS, TSCRIPT, TRESOLVE
-from design.Item import Item
-from design.Job import Job
-from design.Test import Test
-from design.TestJob import TestJob
-from design.TestJobManager import TestJobManager
--- build/lib/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-# pyright: reportUnusedImport=false
-from App import App
-from pages.JobPage import JobPage
-from pages.Page import Page
-from pages.TestPage import TestPage
