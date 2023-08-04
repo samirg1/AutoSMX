f691613115ed5405c6f9709992b0e7457add582c
Samir Gupta <srgupta@bigpond.com>
2023-08-04 18:22:12 +1000

    edit job

--- src/design/Job.py
+++ src/design/Job.py
@@ -1,16 +1,10 @@
-from typing import Literal
-
 from design import Item, Test
 
-TJOB = Literal["CAMPEYN", "BENETAS", "ABLE", "JEWISH"]
-
-
 class Job:
-    def __init__(self, number: str, campus: str, address: str, type: TJOB) -> None:
-        self._number = number
+    def __init__(self, company: str, campus: str, department: str) -> None:
+        self._company = company
         self._campus = campus
-        self._address = address
-        self._type = type
+        self._department = department
         self._tests: list[Test] = []
 
     def add_test(self, item: Item) -> Test:
@@ -18,7 +12,7 @@ class Job:
         return self._tests[-1]
 
     def __str__(self) -> str:
-        return f"{self._number} - {self._campus}\n{self._type}\n{self._address}"
+        return f"{self._campus}\n{self._company}\n{self._department}"
 
     def __hash__(self) -> int:
-        return hash(self._number)
+        return hash(self._campus)
