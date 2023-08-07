4f8e226f908b937fafaa13cc1b74ebb5f54b7a53
Samir Gupta <srgupta@bigpond.com>
2023-08-07 11:18:42 +1000

    added options and decorator to automation functions

--- src/automations.py
+++ src/automations.py
@@ -1,27 +1,60 @@
+import functools
 import os
-import pyautogui
-from pynput import mouse
-import pyperclip # type: ignore
+from typing import Any, Callable
 
+import pyautogui
+import pyperclip  # type: ignore
+from pynput import mouse
+
+_PRINT = True
+_PRINT_ONLY = True
+
+
+def _print(default: Any | None = None) -> Callable[[Callable[..., Any]], Any]:
+    def decorator(func: Callable[..., Any]):
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any):
+            if _PRINT:
+                print(func.__name__, args, kwargs, end=" ")
+                if _PRINT_ONLY:
+                    print("->", default)
+                    return default
+            res = func(*args, **kwargs)
+            if _PRINT:
+                print("->", res)
+            return res
+
+        return wrapper
+
+    return decorator
+
+
+@_print()
 def click_key(*keys: str, times: int = 1):
     for _ in range(times):
         pyautogui.hotkey(*keys, interval=0.1)
         pyautogui.sleep(0.1)
 
-def type(text: str):
-        pyautogui.typewrite(text, interval=0.1)
 
+@_print()
+def type(text: str):
+    pyautogui.typewrite(text, interval=0.1)
+
+
+@_print()
 def click(position: tuple[int, int] | None = None, /, *, times: int = 1):
-    if position is None: # click middle if no position given
+    if position is None:  # click middle if no position given
         x, y = pyautogui.size()
         position = x // 2, y // 2
-
     for _ in range(times):
         pyautogui.click(position)
         pyautogui.sleep(0.1)
 
+
+@_print((0, 0))
 def get_click_position() -> tuple[int, int]:
-    position: tuple[int, int] = -1, -1 
+    position: tuple[int, int] = -1, -1
+
     def on_click(x: float, y: float, button: mouse.Button, pressed: bool):
         if button == mouse.Button.left and pressed:
             nonlocal position
@@ -34,7 +67,9 @@ def get_click_position() -> tuple[int, int]:
 
     return position
 
+
+@_print("selected text")
 def get_selected_text():
-    click_key("ctrl" if os.name == "nt" else "command", "c", times=1)
+    click_key("ctrl" if os.name == "nt" else "command", "c")
     pyautogui.sleep(0.01)
     return pyperclip.paste()
