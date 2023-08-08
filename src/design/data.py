5f27811ce9cc3cdae6532509513bed0fb75bc6c5
Samir Gupta <srgupta@bigpond.com>
2023-08-08 16:39:58 +1000

    make script tests more readable

--- src/design/data.py
+++ src/design/data.py
@@ -2,7 +2,7 @@ _PASS = "Pass"
 _FAIL = "Fail"
 _N_A = "N/A"
 _NO = "No"
-_YES = "Yes"
+_PES = "Yes"
 _ONE = "1"
 _ZERO = "0"
 _SPACE = " "
@@ -15,63 +15,63 @@ class ScriptTest:
         self.options = sorted(options, key=lambda x: -1 if x == _PASS else 0 if x == _N_A else 1)
 
 
-_CASTORS_Y = ScriptTest("CASTORS", _PASS, _N_A, _FAIL)
-_FRAME = ScriptTest("FRAME", _PASS, _N_A, _FAIL)
-_PAINT = ScriptTest("PAINT", _PASS, _N_A, _FAIL)
-_LABELLING = ScriptTest("LABELLING", _PASS, _N_A, _FAIL)
+_CASTORS_P = ScriptTest("CASTORS", _PASS, _N_A, _FAIL)
+_FRAME_P = ScriptTest("FRAME", _PASS, _N_A, _FAIL)
+_PAINT_P = ScriptTest("PAINT", _PASS, _N_A, _FAIL)
+_LABELLING_P = ScriptTest("LABELLING", _PASS, _N_A, _FAIL)
 _CHARGER_N = ScriptTest("CHARGER", _N_A, _PASS, _FAIL)
 _BATTERY_N = ScriptTest("BATTERY", _N_A, _PASS, _FAIL)
-_BATTERY_Y = ScriptTest("BATTERY", _PASS, _N_A, _FAIL)
-_CONTROL = ScriptTest("CONTROL", _PASS, _N_A, _FAIL)
-_CONDITION = ScriptTest("CONDITION", _ONE, _ZERO)
-_FURTHER_ATTENTION = ScriptTest("ATTENTION?", _NO, _YES)
-_DETAILS = ScriptTest("DETAILS", _PASS, _N_A, _FAIL)
-_GENERAL_WEAR = ScriptTest("WEAR", _PASS, _N_A, _FAIL)
-_STITCHING = ScriptTest("STITCHING", _PASS, _N_A, _FAIL)
-_FITTINGS = ScriptTest("FITTINGS", _PASS, _N_A, _FAIL)
+_BATTERY_P = ScriptTest("BATTERY", _PASS, _N_A, _FAIL)
+_CONTROL_P = ScriptTest("CONTROL", _PASS, _N_A, _FAIL)
+_CONDITION_1 = ScriptTest("CONDITION", _ONE, _ZERO)
+_FURTHER_ATTENTION_N = ScriptTest("ATTENTION?", _NO, _PES)
+_DETAILS_P = ScriptTest("DETAILS", _PASS, _N_A, _FAIL)
+_GENERAL_WEAR_P = ScriptTest("WEAR", _PASS, _N_A, _FAIL)
+_STITCHING_P = ScriptTest("STITCHING", _PASS, _N_A, _FAIL)
+_FITTINGS_P = ScriptTest("FITTINGS", _PASS, _N_A, _FAIL)
 _VELCRO_N = ScriptTest("VELCRO", _N_A, _PASS, _FAIL)
-_HARDWARE = ScriptTest("HARDWARE", _PASS, _N_A, _FAIL)
-_OPERATION = ScriptTest("OPERATION", _PASS, _N_A, _FAIL)
-_ATTACHMENTS = ScriptTest("ATTACHMENTS", _N_A, _PASS, _FAIL)
-_STRAP = ScriptTest("STRAP", _PASS, _N_A, _FAIL)
-_ROLLERS = ScriptTest("ROLLERS", _PASS, _N_A, _FAIL)
-_MOTORS = ScriptTest("MOTORS", _PASS, _N_A, _FAIL)
-_CHARGING_CONTACTS = ScriptTest("CONTACTS", _PASS, _N_A, _FAIL)
-_STATUS_LEDS = ScriptTest("STATUS LEDS", _PASS, _N_A, _FAIL)
-_END_LIMIT = ScriptTest("END LIMIT", _PASS, _N_A, _FAIL)
-_SLING_INSPECTION = ScriptTest("SLING", _N_A, _PASS, _FAIL)
-_LOAD_TEST = ScriptTest("LOAD TEST", _PASS, _N_A, _FAIL)
-_EMERGENCY_OFF = ScriptTest("EMERGENCY OFF", _PASS, _N_A, _FAIL)
-_MECHANICAL_LOWERING = ScriptTest("MECH LOWERING", _PASS, _N_A, _FAIL)
-_ELECTRICAL_LOWERING = ScriptTest("ELEC LOWERING", _PASS, _N_A, _FAIL)
-_ELECTRICAL_LIFTING = ScriptTest("ELEC LIFTING", _PASS, _N_A, _FAIL)
-_RESET = ScriptTest("RESET", _N_A, _PASS, _FAIL)
+_HARDWARE_P = ScriptTest("HARDWARE", _PASS, _N_A, _FAIL)
+_OPERATION_P = ScriptTest("OPERATION", _PASS, _N_A, _FAIL)
+_ATTACHMENTS_N = ScriptTest("ATTACHMENTS", _N_A, _PASS, _FAIL)
+_STRAP_P = ScriptTest("STRAP", _PASS, _N_A, _FAIL)
+_ROLLERS_P = ScriptTest("ROLLERS", _PASS, _N_A, _FAIL)
+_MOTORS_P = ScriptTest("MOTORS", _PASS, _N_A, _FAIL)
+_CHARGING_CONTACTS_P = ScriptTest("CONTACTS", _PASS, _N_A, _FAIL)
+_STATUS_LEDS_P = ScriptTest("STATUS LEDS", _PASS, _N_A, _FAIL)
+_END_LIMIT_P = ScriptTest("END LIMIT", _PASS, _N_A, _FAIL)
+_SLING_INSPECTION_N = ScriptTest("SLING", _N_A, _PASS, _FAIL)
+_LOAD_TEST_P = ScriptTest("LOAD TEST", _PASS, _N_A, _FAIL)
+_EMERGENCY_OFF_P = ScriptTest("EMERGENCY OFF", _PASS, _N_A, _FAIL)
+_MECHANICAL_LOWERING_P = ScriptTest("MECH LOWERING", _PASS, _N_A, _FAIL)
+_ELECTRICAL_LOWERING_P = ScriptTest("ELEC LOWERING", _PASS, _N_A, _FAIL)
+_ELECTRICAL_LIFTING_P = ScriptTest("ELEC LIFTING", _PASS, _N_A, _FAIL)
+_RESET_N = ScriptTest("RESET", _N_A, _PASS, _FAIL)
 _NUMBER_OF_LIFTS = ScriptTest("#LIFTS")
-_USAGE_ENVIRONMENT = ScriptTest("ENVIRONMENT", _NO, _YES)
-_FEATURES = ScriptTest("FEATURES", _PASS, _N_A, _FAIL)
-_TRACK_DUST = ScriptTest("TRACK DUST", _PASS, _N_A, _FAIL)
+_USAGE_ENVIRONMENT_N = ScriptTest("ENVIRONMENT", _NO, _PES)
+_FEATURES_P = ScriptTest("FEATURES", _PASS, _N_A, _FAIL)
+_TRACK_DUST_P = ScriptTest("TRACK DUST", _PASS, _N_A, _FAIL)
 _TRACK_LOAD_KG = ScriptTest("TRACK LOAD", "150")
-_ELECTRIC_TEST = ScriptTest("ELECTRICALS", _N_A, _PASS, _FAIL)
-_HELP_POLE = ScriptTest("HELP POLE", _N_A, _PASS, _FAIL)
-_VISUAL = ScriptTest("VISUAL", _PASS, _N_A, _FAIL)
-_PIVOTS = ScriptTest("PIVOTS", _PASS, _N_A, _FAIL)
-_SWIVEL = ScriptTest("SWIVEL", _PASS, _N_A, _FAIL)
-_ACCURACY = ScriptTest("ACCURACY", _PASS, _N_A, _FAIL)
-_SEAT_FUNCTIONS = ScriptTest("SEAT FUNC", _PASS, _N_A, _FAIL)
+_ELECTRIC_TEST_N = ScriptTest("ELECTRICALS", _N_A, _PASS, _FAIL)
+_HELP_POLE_N = ScriptTest("HELP POLE", _N_A, _PASS, _FAIL)
+_VISUAL_P = ScriptTest("VISUAL", _PASS, _N_A, _FAIL)
+_PIVOTS_P = ScriptTest("PIVOTS", _PASS, _N_A, _FAIL)
+_SWIVEL_P = ScriptTest("SWIVEL", _PASS, _N_A, _FAIL)
+_ACCURACY_P = ScriptTest("ACCURACY", _PASS, _N_A, _FAIL)
+_SEAT_FUNCTIONS_P = ScriptTest("SEAT FUNC", _PASS, _N_A, _FAIL)
 _ELECTRIC_N = ScriptTest("ELECTRIC", _N_A, _PASS, _FAIL)
-_CAPACITY = ScriptTest("CAPACITY", _PASS, _N_A, _FAIL)
-_DATE = ScriptTest("DATE", _PASS, _N_A, _FAIL)
-_MAINS = ScriptTest("MAINS", _PASS, _N_A, _FAIL)
-_FUSES = ScriptTest("FUSES", _PASS, _N_A, _FAIL)
+_CAPACITY_P = ScriptTest("CAPACITY", _PASS, _N_A, _FAIL)
+_DATE_P = ScriptTest("DATE", _PASS, _N_A, _FAIL)
+_MAINS_P = ScriptTest("MAINS", _PASS, _N_A, _FAIL)
+_FUSES_P = ScriptTest("FUSES", _PASS, _N_A, _FAIL)
 _EARTH_RESISTANCE = ScriptTest("EARTH R", _SPACE)
 _INSULATION_RESISTANCE = ScriptTest("INSULATION R", "199")
 _INSULATION_RESISTANCE_ENCLOSURE = ScriptTest("IRE", "199")
 _TOUCH_CURRENT = ScriptTest("TOUCH CURRENT", _SPACE)
 _IEC_MAINS_LEAD = ScriptTest("IEC MAINS")
-_MISSING_COMPONENTS = ScriptTest("MISSING", _NO, _YES)
-_POLES = ScriptTest("POLES", _PASS, _N_A, _FAIL)
-_CLEAN = ScriptTest("CLEAN", _PASS, _N_A, _FAIL)
-_PERFORMANCE = ScriptTest("PERFORMANCE", _PASS, _N_A, _FAIL)
+_MISSING_COMPONENTS_N = ScriptTest("MISSING", _NO, _PES)
+_POLES_P = ScriptTest("POLES", _PASS, _N_A, _FAIL)
+_CLEAN_P = ScriptTest("CLEAN", _PASS, _N_A, _FAIL)
+_PERFORMANCE_P = ScriptTest("PERFORMANCE", _PASS, _N_A, _FAIL)
 
 
 class Script:
@@ -92,119 +92,119 @@ class Script:
 
 SCRIPTS: dict[str, Script] = {
     "CHANGE TILT TABLE": Script(
-        "CHANGE TILT TABLE", "AT - CHANGE / TILT TABLE, ELECTRIC/MANUAL", 0, _CASTORS_Y, _FRAME, _PAINT, _LABELLING, _CHARGER_N, _BATTERY_N, _CONTROL, _CONDITION, _FURTHER_ATTENTION
+        "CHANGE TILT TABLE", "AT - CHANGE / TILT TABLE, ELECTRIC/MANUAL", 0, _CASTORS_P, _FRAME_P, _PAINT_P, _LABELLING_P, _CHARGER_N, _BATTERY_N, _CONTROL_P, _CONDITION_1, _FURTHER_ATTENTION_N
     ),
-    "SLING": Script("SLING", "AT - SLING", 1, _DETAILS, _GENERAL_WEAR, _STITCHING, _FITTINGS, _VELCRO_N, _LABELLING, _CONDITION),
+    "SLING": Script("SLING", "AT - SLING", 1, _DETAILS_P, _GENERAL_WEAR_P, _STITCHING_P, _FITTINGS_P, _VELCRO_N, _LABELLING_P, _CONDITION_1),
     "WALKER": Script(
         "WALKER",
         "AT - WALKER / STANDER",
         2,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _FRAME,
-        _HARDWARE,
-        _PAINT,
-        _OPERATION,
-        _ATTACHMENTS,
-        _LABELLING,
-        _CONDITION,
-        _FURTHER_ATTENTION,
+        _CASTORS_P,
+        _CASTORS_P,
+        _CASTORS_P,
+        _FRAME_P,
+        _HARDWARE_P,
+        _PAINT_P,
+        _OPERATION_P,
+        _ATTACHMENTS_N,
+        _LABELLING_P,
+        _CONDITION_1,
+        _FURTHER_ATTENTION_N,
         extra_terms=["WALK", "STAND", "STEDY"],
     ),
     "CEILING HOIST": Script(
         "CEILING",
         "AT - CEILING HOIST",
         3,
-        _OPERATION,
-        _CONTROL,
-        _STRAP,
-        _ROLLERS,
-        _MOTORS,
+        _OPERATION_P,
+        _CONTROL_P,
+        _STRAP_P,
+        _ROLLERS_P,
+        _MOTORS_P,
         _CHARGER_N,
-        _BATTERY_Y,
-        _CHARGING_CONTACTS,
-        _STATUS_LEDS,
-        _END_LIMIT,
-        _LABELLING,
-        _HARDWARE,
-        _SLING_INSPECTION,
-        _LOAD_TEST,
-        _EMERGENCY_OFF,
-        _MECHANICAL_LOWERING,
-        _ELECTRICAL_LOWERING,
-        _RESET,
+        _BATTERY_P,
+        _CHARGING_CONTACTS_P,
+        _STATUS_LEDS_P,
+        _END_LIMIT_P,
+        _LABELLING_P,
+        _HARDWARE_P,
+        _SLING_INSPECTION_N,
+        _LOAD_TEST_P,
+        _EMERGENCY_OFF_P,
+        _MECHANICAL_LOWERING_P,
+        _ELECTRICAL_LOWERING_P,
+        _RESET_N,
         _NUMBER_OF_LIFTS,
-        _CONDITION,
-        _USAGE_ENVIRONMENT,
+        _CONDITION_1,
+        _USAGE_ENVIRONMENT_N,
     ),
-    "TRACK": Script("TRACK", "AT - TRACK", 4, _FEATURES, _TRACK_DUST, _CHARGER_N, _TRACK_LOAD_KG, _LOAD_TEST, _ELECTRIC_TEST),
-    "COMMODE": Script("COMMODE", "AT - COMMODE", 5, _CASTORS_Y, _FRAME, _PAINT, _OPERATION, _LABELLING, _CHARGER_N, _BATTERY_N, _CONTROL, _CONDITION),
-    "BED": Script("BED", "AT - ELECTRIC BED", 6, _CASTORS_Y, _FRAME, _HARDWARE, _CONTROL, _PAINT, _OPERATION, _LABELLING, _CHARGER_N, _HELP_POLE, _CONDITION),
+    "TRACK": Script("TRACK", "AT - TRACK", 4, _FEATURES_P, _TRACK_DUST_P, _CHARGER_N, _TRACK_LOAD_KG, _LOAD_TEST_P, _ELECTRIC_TEST_N),
+    "COMMODE": Script("COMMODE", "AT - COMMODE", 5, _CASTORS_P, _FRAME_P, _PAINT_P, _OPERATION_P, _LABELLING_P, _CHARGER_N, _BATTERY_N, _CONTROL_P, _CONDITION_1),
+    "BED": Script("BED", "AT - ELECTRIC BED", 6, _CASTORS_P, _FRAME_P, _HARDWARE_P, _CONTROL_P, _PAINT_P, _OPERATION_P, _LABELLING_P, _CHARGER_N, _HELP_POLE_N, _CONDITION_1),
     "FLOOR HOIST": Script(
         "FLOOR",
         "AT - FLOOR HOIST",
         7,
-        _CASTORS_Y,
-        _VISUAL,
-        _PIVOTS,
-        _PAINT,
-        _OPERATION,
-        _HARDWARE,
+        _CASTORS_P,
+        _VISUAL_P,
+        _PIVOTS_P,
+        _PAINT_P,
+        _OPERATION_P,
+        _HARDWARE_P,
         _CHARGER_N,
         _BATTERY_N,
-        _CONTROL,
-        _SWIVEL,
-        _LOAD_TEST,
-        _EMERGENCY_OFF,
-        _MECHANICAL_LOWERING,
-        _ELECTRICAL_LOWERING,
-        _ELECTRICAL_LIFTING,
-        _RESET,
-        _CONDITION,
-        _USAGE_ENVIRONMENT,
+        _CONTROL_P,
+        _SWIVEL_P,
+        _LOAD_TEST_P,
+        _EMERGENCY_OFF_P,
+        _MECHANICAL_LOWERING_P,
+        _ELECTRICAL_LOWERING_P,
+        _ELECTRICAL_LIFTING_P,
+        _RESET_N,
+        _CONDITION_1,
+        _USAGE_ENVIRONMENT_N,
         extra_terms=["LIFTS"],
     ),
-    "TUB": Script("TUB", "AT - TUB / BATH CHAIRS", 8, _CASTORS_Y, _FRAME, _PAINT, _OPERATION, _LABELLING, _CHARGER_N, _BATTERY_N, _CONTROL, _CONDITION, extra_terms=["BATH"]),
-    "SCALE": Script("SCALE", "AT - WEIGH CHAIR / SCALE", 9, _CASTORS_Y, _FRAME, _PAINT, _OPERATION, _LABELLING, _CHARGER_N, _BATTERY_Y, _ACCURACY, _CONDITION, extra_terms=["WEIGH"]),
+    "TUB": Script("TUB", "AT - TUB / BATH CHAIRS", 8, _CASTORS_P, _FRAME_P, _PAINT_P, _OPERATION_P, _LABELLING_P, _CHARGER_N, _BATTERY_N, _CONTROL_P, _CONDITION_1, extra_terms=["BATH"]),
+    "SCALE": Script("SCALE", "AT - WEIGH CHAIR / SCALE", 9, _CASTORS_P, _FRAME_P, _PAINT_P, _OPERATION_P, _LABELLING_P, _CHARGER_N, _BATTERY_P, _ACCURACY_P, _CONDITION_1, extra_terms=["WEIGH"]),
     "WHEELCHAIR": Script(
         "WHEELCHAIR",
         "AT - WHEELCHAIR",
         10,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _CASTORS_Y,
-        _PAINT,
-        _FRAME,
-        _FRAME,
-        _HARDWARE,
-        _ATTACHMENTS,
-        _PAINT,
-        _SEAT_FUNCTIONS,
-        _OPERATION,
-        _OPERATION,
-        _ATTACHMENTS,
-        _LABELLING,
+        _CASTORS_P,
+        _CASTORS_P,
+        _CASTORS_P,
+        _CASTORS_P,
+        _CASTORS_P,
+        _PAINT_P,
+        _FRAME_P,
+        _FRAME_P,
+        _HARDWARE_P,
+        _ATTACHMENTS_N,
+        _PAINT_P,
+        _SEAT_FUNCTIONS_P,
+        _OPERATION_P,
+        _OPERATION_P,
+        _ATTACHMENTS_N,
+        _LABELLING_P,
         _CHARGER_N,
         _BATTERY_N,
         _ELECTRIC_N,
         _ELECTRIC_N,
-        _CONDITION,
+        _CONDITION_1,
     ),
-    "CHARGER": Script("CHARGER", "BATTERY OPERATED / VISUAL TEST", 11, _BATTERY_N, _CONTROL, _VISUAL, _ATTACHMENTS),
-    "BATTERY": Script("BATTERY", "BATTERY PACK", 12, _VISUAL, _CAPACITY, _DATE),
+    "CHARGER": Script("CHARGER", "BATTERY OPERATED / VISUAL TEST", 11, _BATTERY_N, _CONTROL_P, _VISUAL_P, _ATTACHMENTS_N),
+    "BATTERY": Script("BATTERY", "BATTERY PACK", 12, _VISUAL_P, _CAPACITY_P, _DATE_P),
     "CLASS II": Script(
         "CLASS II",
         "CLASS II NO APPLIED PARTS",
         13,
-        _MAINS,
-        _FUSES,
-        _CONTROL,
+        _MAINS_P,
+        _FUSES_P,
+        _CONTROL_P,
         _BATTERY_N,
-        _VISUAL,
-        _ATTACHMENTS,
+        _VISUAL_P,
+        _ATTACHMENTS_N,
         _EARTH_RESISTANCE,
         _INSULATION_RESISTANCE,
         _INSULATION_RESISTANCE_ENCLOSURE,
@@ -212,6 +212,6 @@ SCRIPTS: dict[str, Script] = {
         _IEC_MAINS_LEAD,
     ),
     "FURNITURE": Script(
-        "FURNITURE", "FURNITURE GENERIC", 14, _CONDITION, _CASTORS_Y, _CASTORS_Y, _PAINT, _CASTORS_Y, _MISSING_COMPONENTS, _POLES, _CLEAN, _ELECTRIC_N, _PERFORMANCE, extra_terms=["OVERBED"]
+        "FURNITURE", "FURNITURE GENERIC", 14, _CONDITION_1, _CASTORS_P, _CASTORS_P, _PAINT_P, _CASTORS_P, _MISSING_COMPONENTS_N, _POLES_P, _CLEAN_P, _ELECTRIC_N, _PERFORMANCE_P, extra_terms=["OVERBED"]
     ),
 }
