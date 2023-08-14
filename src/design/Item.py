5a34bbfed81b7e9941357114964a422ef42d2a11
Samir Gupta <srgupta@bigpond.com>
2023-08-14 21:04:28 +1000

    converted script into dataclass

--- src/design/Item.py
+++ src/design/Item.py
@@ -4,12 +4,12 @@ from dataclasses import dataclass, field
 @dataclass(frozen=True, repr=False, slots=True, order=True)
 class Item:
     number: str = field(hash=True)
-    description: str = field(compare=False, hash=False)
-    model: str = field(compare=False, hash=False)
-    manufacturer: str = field(compare=False, hash=False)
-    room: str = field(compare=False, hash=False)
-    last_service: str = field(compare=False, hash=False)
-    serial: str = field(compare=False, hash=False)
+    description: str = field(compare=False)
+    model: str = field(compare=False)
+    manufacturer: str = field(compare=False)
+    room: str = field(compare=False)
+    last_service: str = field(compare=False)
+    serial: str = field(compare=False)
 
     def __str__(self) -> str:
         return f"{self.number} - {self.description}"
