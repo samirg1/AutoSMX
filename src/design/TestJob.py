fdb7f1dfd0417e24df933a96c214c4e7ae27fc45
Samir Gupta <srgupta@bigpond.com>
2023-08-23 15:59:13 +1000

    add separate testjob comment for tests that doent include part

--- src/design/TestJob.py
+++ src/design/TestJob.py
@@ -3,6 +3,18 @@ class TestJob:
         self.department = department
         self.contact_name = contact_name
         self.comment = comment.strip()
+        self.test_comment = self.comment
+        self.part_quantity = 0
+        self.part_number = ""
+
+        lines = self.comment.split("\n")
+        match lines[-1].split(" "):
+            case [quantity, "X" | "x", part_number] if quantity.isdigit():
+                self.part_quantity = int(quantity)
+                self.part_number = part_number
+                self.test_comment = "\n".join(lines[:-1])
+            case _:
+                pass
 
     def __str__(self) -> str:
         return f"{self.comment}"
