2fc29b549142fa8e3a00acee27ba93061b26d049
Samir Gupta <srgupta@bigpond.com>
2023-08-29 15:54:04 +1000

    fix tests

--- testing/design/test_job.py
+++ testing/design/test_job.py
@@ -22,10 +22,10 @@ def test_job_add_test():
     test1 = Test(Item("001", "Test Item 1", "ModelX", "ManufacturerX", "XYZ001"))
     test2 = Test(Item("002", "Test Item 2", "ModelY", "ManufacturerY", "XYZ002"))
 
-    custom1 = Script("Custom1", "Custom Script", 2, (), exact_matches=["Test Item 1"])
+    custom1 = Script("Custom1", "Custom Script", (), exact_matches=["Test Item 1"])
     SCRIPTS["Custom1"] = custom1
     test1.script = test1.determine_script()
-    custom2 = Script("Custom2", "Custom Script", 2, (), exact_matches=["Test Item 2"])
+    custom2 = Script("Custom2", "Custom Script", (), exact_matches=["Test Item 2"])
     SCRIPTS["Custom2"] = custom2
     test2.script = test2.determine_script()
 
