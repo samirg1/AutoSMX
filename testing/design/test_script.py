166df90b6d84c23a660d028814e0ce1b884d2cc3
Samir Gupta <srgupta@bigpond.com>
2023-08-29 15:54:04 +1000

    fix tests

--- testing/design/test_script.py
+++ testing/design/test_script.py
@@ -5,10 +5,9 @@ from design.Script import Script, ScriptTest
 
 
 def test_script_creation():
-    script = Script("Nick", "Script Name", 3)
+    script = Script("Nick", "Script Name")
     assert script.nickname == "Nick"
     assert script.name == "Script Name"
-    assert script.downs == 3
     assert len(script.tests) == 0
     assert script.search_terms == ["Nick"]
 
@@ -16,19 +15,17 @@ def test_script_creation():
 def test_script_creation_with_tests():
     test1 = ScriptTest("Test 1", "Pass", "Fail")
     test2 = ScriptTest("Test 2", "N/A", "Yes", "No")
-    script = Script("Nickname", "Test Script", 2, (test1, test2))
+    script = Script("Nickname", "Test Script", (test1, test2))
     assert script.nickname == "Nickname"
     assert script.name == "Test Script"
-    assert script.downs == 2
     assert len(script.tests) == 2
     assert script.search_terms == ["Nickname"]
 
 
 def test_script_creation_with_extra_terms():
-    script = Script("UniqueNick", "Unique Script", 1, search_terms=["tag1", "tag2"])
+    script = Script("UniqueNick", "Unique Script", search_terms=["tag1", "tag2"])
     assert script.nickname == "UniqueNick"
     assert script.name == "Unique Script"
-    assert script.downs == 1
     assert len(script.tests) == 0
     assert script.search_terms == ["tag1", "tag2", "UniqueNick"]
 
@@ -44,19 +41,19 @@ def test_script_creation_with_extra_terms():
     ],
 )
 def test_script_matches(match_description: str, expected: bool):
-    script = Script("Tester", "Matching Script", 0, search_terms=["tag3"])
+    script = Script("Tester", "Matching Script", search_terms=["tag3"])
     assert script.is_for(match_description) == expected
 
 
 def test_script_to_string():
-    script = Script("MyNick", "Awesome Script", 5)
+    script = Script("MyNick", "Awesome Script")
     assert str(script) == "Awesome Script"
 
 
 def test_hash_and_eq():
-    script1 = Script("Nick1", "Script 1", 1)
-    script2 = Script("Nick2", "Script 2", 2)
-    script3 = Script("Nick3", "Script 1", 3)
+    script1 = Script("Nick1", "Script 1")
+    script2 = Script("Nick2", "Script 2")
+    script3 = Script("Nick3", "Script 1")
     assert len({script1, script2}) == 2
     assert len({script1, script3}) == 1
     assert hash(script1) != hash(script2)
