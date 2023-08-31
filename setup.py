e1cb65b597d7fa4ab6fe8c8ebd477e6d0cae22b0
Samir Gupta <srgupta@bigpond.com>
2023-08-31 22:25:12 +1000

    include the png in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ build_exe_options = {
     "excludes": ["pytest", "PyQt6", "PyQt5", "cv2", "numpy", "mypy"],
     "packages": ["pyautogui", "pyperclip", "attrs", "pynput"],
     "include_msvcr": True,
-    "include_files": "src/storage/store.json",
+    "include_files": ["src/storage/store.json", "a.png"],
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
