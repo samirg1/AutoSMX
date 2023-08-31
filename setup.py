78c7f2e1d98bbbfd018fdbb366f2a6e32bccf129
Samir Gupta <srgupta@bigpond.com>
2023-08-31 22:08:41 +1000

    add icon

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import sys
 
 from cx_Freeze import Executable, setup  # type: ignore
 
-# Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "excludes": ["pytest", "PyQt6", "PyQt5", "cv2", "numpy", "mypy"],
     "packages": ["pyautogui", "pyperclip", "attrs", "pynput"],
@@ -10,7 +9,6 @@ build_exe_options = {
     "include_files": "src/storage/store.json",
 }
 
-# base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
@@ -18,5 +16,5 @@ setup(
     version="1.0",
     description="GUI helper application for ALTER's SMX software",
     options={"build_exe": build_exe_options},
-    executables=[Executable("src/App.py", base=base, target_name="ALTER SMX Tool")],
+    executables=[Executable("src/App.py", base=base, target_name="ALTER SMX Tool", icon="a.ico")],
 )
