8314722b3a6c0c2d516f1d8ddec051c9f08b0fd3
Samir Gupta <srgupta@bigpond.com>
2023-09-01 17:57:21 +1000

    try to restore tox

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-import sys
+from setuptools import setup
 
-from cx_Freeze import Executable, setup  # type: ignore
-
-build_exe = {
-    "excludes": ["pytest", "PyQt6", "PyQt5", "cv2", "numpy", "mypy", "test", "email", "pydoc_data", "multiprocessing", "rubicon"],
-    "packages": ["pyautogui", "pyperclip", "attrs", "pynput"],
-    "include_msvcr": True,
-    "include_files": ["autosmx.png"],
-}
-
-bdist_mac = {"iconfile": "autosmx.ico", "bundle_name": "AutoSMX"}
-
-base = "Win32GUI" if sys.platform == "win32" else None
-
-setup(
-    name="AutoSMX",
-    version="1.0.0",
-    description="GUI Automater for SMX",
-    author="Samir Gupta",
-    options={"build_exe": build_exe, "bdist_mac": bdist_mac},
-    executables=[Executable("src/App.py", base=base, target_name="AutoSMX", icon="autosmx.ico")],
-)
+if __name__ == "__main__":
+    setup()
