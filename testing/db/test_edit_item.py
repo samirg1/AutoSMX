2ae6040a5647de4b01d9b414ef38f7885265072b
Samir Gupta <srgupta@bigpond.com>
2023-11-06 19:44:26 +1100

    minor formatting edit

--- testing/db/test_edit_item.py
+++ testing/db/test_edit_item.py
@@ -12,7 +12,7 @@ def test_get_item(mock_sql_connect: MockSqlObject) -> None:
     edit_item("12345", {"room": "Room 1"})
 
     assert len(mock_sql_connect.calls) == 1
-    (sql, variables), = mock_sql_connect.calls
+    ((sql, variables),) = mock_sql_connect.calls
     assert "room = ?" in sql
     assert len(variables) == 4
     assert variables[2] == "Room 1"
